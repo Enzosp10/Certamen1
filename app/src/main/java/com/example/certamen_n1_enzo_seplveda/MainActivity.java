3815a444fbd96044d8c2ffbcf22f733a4839cfd1
Enzo <enzo.virginiogomez.cl>
2021-10-16 11:33:47 -0300

    Pintando_01

---
+++ app/src/main/java/com/example/certamen_n1_enzo_seplveda/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.certamen_n1_enzo_seplveda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
