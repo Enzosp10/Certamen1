c1669c0167d02fb2f68fff5b1ec465f1a9a5d2cc
Enzo <enzo.virginiogomez.cl>
2021-10-20 19:01:06 -0300

    Codigo001

--- app/src/main/java/com/example/certamen_n1_enzo_seplveda/MainActivity.java
+++ app/src/main/java/com/example/certamen_n1_enzo_seplveda/MainActivity.java
@@ -6,22 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
-    Button Btnaceptar;
+
+     Button btncalc;
+     EditText Nombre;
+     EditText Carrera;
+     TextView Resultado;
+     EditText nota1;
+     EditText nota2;
+     EditText nota3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nota1 = (EditText) findViewById(R.id.nota1);
+        nota2 = (EditText) findViewById(R.id.nota2);
+        nota3 = (EditText) findViewById(R.id.nota3);
+        Intent act = new Intent(this,MainActivity2.class);
+        Nombre = (EditText)findViewById(R.id.eTnombre);
+        Carrera = (EditText)findViewById(R.id.eTcarrera);
 
-        Btnaceptar = (Button)findViewById(R.id.btnAceptar);
-        Btnaceptar.setOnClickListener(new View.OnClickListener() {
+        btncalc = (Button)findViewById(R.id.btncalc1);
+
+        btncalc.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Intent Btnaceptar = new Intent(MainActivity.this,MainActivity2.class);
-                startActivity(Btnaceptar);
+
+                float Resultado=(Float.parseFloat(nota1.getText().toString())+Float.parseFloat(nota2.getText().toString())+Float.parseFloat(nota3.getText().toString()))/3;
+                act.putExtra("Nombre",Nombre.getText().toString());
+                act.putExtra("Carrera"," ");
+                act.putExtra("Resultado",Resultado);
+
+                Intent btncalc = new Intent(MainActivity.this,MainActivity2.class);
+
+                startActivity(act);
             }
         });
     }
--- app/src/main/java/com/example/certamen_n1_enzo_seplveda/MainActivity2.java
+++ app/src/main/java/com/example/certamen_n1_enzo_seplveda/MainActivity2.java
@@ -2,10 +2,18 @@ package com.example.certamen_n1_enzo_seplveda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+     private Button Btnvolver;
+     private TextView Nom;
+     private TextView Prom;
+     private TextView Info;
 
 
 
@@ -14,10 +22,40 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        RecibirDatos();
+        Btnvolver = (Button)findViewById(R.id.button2);
+
+        Bundle extras = getIntent().getExtras();
+        String N1 = extras.getString("Nombre");
+        String C1 = extras.getString("Carrera");
+        Float R1 = extras.getFloat("Resultado");
+
+        Nom= (TextView) findViewById(R.id.NombreEstudiante);
+        Nom.setText(N1);
+        Prom= (TextView) findViewById(R.id.Promedio);
+        Prom.setText(R1.toString());
+        Info= (TextView) findViewById(R.id.eTinformacion);
+        Info.setText(N1+" Que estudia  "+R1);
+
+        Btnvolver.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent Btnvolver = new Intent(MainActivity2.this,MainActivity.class);
+
+                startActivity(Btnvolver);
+            }
+        });
 
 
 
 
+
+    }
+
+
+    public void  RecibirDatos(){
+        Bundle extras = getIntent().getExtras();
 
     }
 }
